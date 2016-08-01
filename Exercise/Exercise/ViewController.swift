51437e39b0e1d9a981530698bb1e8a38426cef77
saikrishna <vicky.raparthi@gmail.com>
2016-08-01 15:55:45 -0500

    new files

---
+++ Exercise/Exercise/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Exercise
+//
+//  Created by saikrishna raparthi on 7/19/16.
+//  Copyright © 2016 saikrishna raparthi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
